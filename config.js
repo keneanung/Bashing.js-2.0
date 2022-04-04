d0ff8715bdfc121246833b42f63c3c052cfebd2f
keneanung <keneanung@googlemail.com>
2022-04-04 21:52:51 +0200

    Initial configuration

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // Set up your project here
 const author = 'keneanung';  // author name on GitHub, might also be an org
-const projectName = 'iron-realms-nexus-script-template'; // project name on GitHub
-const libraryName = 'Greeter'; // name of the resulting library. Used by webpack to create a global name
-const npmPackageName = 'nexus_script_template'; // Name of the NPM package that should be published
-const projectDescription = 'Template for nexus scripts using typescript'; // Project description in the NPM package
+const projectName = 'Bashing.js-2.0'; // project name on GitHub
+const libraryName = 'Bashing'; // name of the resulting library. Used by webpack to create a global name
+const npmPackageName = 'bashing-js-2'; // Name of the NPM package that should be published
+const projectDescription = 'Second generation Bashing script for IRE Nexus'; // Project description in the NPM package
 const authorEmail = 'keneanung@gmail.com' // author email address
-const docuTitle = 'Iron Realms Nexus Script Template'; // Title of the documentation website
-const docuTagline = 'Making Nexus scripts easier'; // Used for documentation website
+const docuTitle = 'Second Generation Bashin in Nexus'; // Title of the documentation website
+const docuTagline = 'All new Bashing package'; // Used for documentation website
 
 
 // don't modify the exports below (except to add new fields)
