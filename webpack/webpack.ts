53a77537556b82df98a8c4e91146c2920d3924ec
keneanung <keneanung@googlemail.com>
2022-04-24 23:47:42 +0200

    Sync workflow with script template

--- webpack/webpack.ts
+++ webpack/webpack.ts
@@ -1,10 +1,6 @@
 // Webpack entrypoint.
 // Use this to do any browser specific initialization and export the module as global object.
 
-import { Greeter } from "../src/index"
+import { Bashing } from "../src/index"
 
-console.log(Greeter("world"));
-
-export {
-    Greeter
-};
+export default Bashing;
